3491eaa6bfc78095c5d8e79a3ebfbcd7ec7750c1
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2025-10-09 16:54:59 +0200

    Update TranslatingService.java

--- backend-quarkus/src/main/java/dev/rabauer/ai/demo/backend/quarkus/TranslatingService.java
+++ backend-quarkus/src/main/java/dev/rabauer/ai/demo/backend/quarkus/TranslatingService.java
@@ -3,8 +3,8 @@ package dev.rabauer.ai.demo.backend.quarkus;
 import dev.langchain4j.service.UserMessage;
 import io.quarkiverse.langchain4j.RegisterAiService;
 
+@RegisterAiService
 public interface TranslatingService {
-
     @UserMessage("You are a translator: translate the following text to {targetLanguage}: {text}")
     String translate(String targetLanguage, String text);
 }
