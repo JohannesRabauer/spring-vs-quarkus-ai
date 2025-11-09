7f06d2d291c3aef1386d7174f54d8a202ad3717b
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2025-11-09 16:58:45 +0100

    Remove stuff to live code

--- backend-spring/src/main/java/dev/rabauer/ai/demo/backend/spring/db/UserComplaintRepository.java
+++
@@ -1,6 +0,0 @@
-package dev.rabauer.ai.demo.backend.spring.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserComplaintRepository extends JpaRepository<UserComplaint, Long> {
-}
